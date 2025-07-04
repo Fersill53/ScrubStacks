c0af8399c4af229fd57dee0b58032fc962f96e75
Fernando Siller <fersill53@gmail.com>
2025-07-04 17:27:43 -0500

    moved env where it should be

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/cards', require('./routes/cards'));
+app.use('/api/cards', require('./server/routes/cards'));
 
 // Database connection
 mongoose.connect(process.env.MONGO_URI, {
