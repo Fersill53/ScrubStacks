1d7b539fdcad32e2d5cbf9101ff5b40cebe1c108
Fernando Siller <fersill53@gmail.com>
2025-07-04 13:08:53 -0500

    updated app.jsx to use react router

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,21 @@
-import CardList from './components/ViewCards';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ViewCards from './components/ViewCards';
+import AddCard from './components/AddCard';
+import EditCards from './components/EditCards';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ScrubStack</h1>
-      <CardList />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/view" element={<ViewCards />} />
+        <Route path="/add" element={<AddCard />} />
+        <Route path="/edit" element={<EditCards />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
