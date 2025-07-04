cd44ab56f4600134b60ccef0e2f92237d694fdc8
Fernando Siller <fersill53@gmail.com>
2025-07-04 11:37:25 -0500

    added code to home.jsx

---
+++ Client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div style={{ padding: '20px'}}>
+            <h1>Welcome to ScrubStack</h1>
+            <p>Manage your surgeon preference cards easily</p>
+        </div>
+    );
+}
+
+export default Home;
