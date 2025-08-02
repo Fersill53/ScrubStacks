ae351843c733c7333091ec894f5ca082aab3768e
Fernando Siller <fersill53@gmail.com>
2025-08-01 19:54:52 -0500

    gave full page card ability to edit all fields

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,6 +1,91 @@
 const mongoose = require('mongoose');
+const { route } = require('../routes/cards');
+
+const instrumetSchema = new mongoose.Schema({
+  qty: Number,
+  name: String,
+  notes: String,
+}, { _id: false });
+
+const supplySchema = new mongoose.Schema({
+  qty: Number,
+  name: String,
+  sizeOrNotes: String,
+}, { _id: false });
+
+const medicationSchema = new mongoose.Schema({
+  name: String,
+  dose: String,
+  route: String,
+  timing: String
+}, { _id: false });
+
+const sutureSchema = new mongoose.Schema({
+  type: String,
+  size: String,
+  needle: String,
+  useSite: String,
+} { _id: false});
 
 const CardSchema = new mongoose.Schema({
+  surgeonName: String,
+  specialty: String,
+  procedure: String,
+  ProcedureCode: String,
+  surgicalServiceLine: String,
+  anesthesiaType: String,
+  estimatedCaseDuration: String,
+  positioning: String,
+  positioningAids: String,
+
+  roomType: String,
+  tableType: String,
+  bedOrientation: String,
+  specialEquipment: [String],
+
+  prepArea: String,
+  prepSolution: String,
+  hairRemoval: String,
+  catheter: String,
+  dvtProphylaxis: [String],
+
+  drapes: [String],
+  customDrapingInstructions: String,
+
+  instruments: [instrumentSchema],
+  implantsDevicesReps: {
+    vendor: String,
+    implant: String,
+    repNamePhone: String,
+    repRequired: Boolean
+  },
+
+  supplies: [supplySchema],
+  medications: [medicationSchema],
+  sutures: [sutureSchema],
+
+  closing: {
+    fascia: String,
+    subcuticular: String,
+    skinClosure: String
+  },
+
+  notes: String,
+
+}, { timestamps: true});
+
+function getCardModel(Connection) {
+  try {
+    return connection.model('Card');
+  } catch (e) {
+    return connection.model('Card', CardSchema, 'cards');
+  }
+}
+
+module.exports = getCardModel;
+
+//Replacing this previous version with what is above.
+/*const CardSchema = new mongoose.Schema({
   surgeonName: { type: String, required: true },
   procedure: { type: String, required: true },
   instruments: [String],
@@ -15,4 +100,5 @@ function getCardModel(connection) {
   }
 }
 
-module.exports = getCardModel;
+module.exports = getCardModel; */
+
